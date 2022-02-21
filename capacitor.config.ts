146cf1ad42c4a8c1e235d53af9d18846517405eb
amepro <amadoumacbookpro38@gmail.com>
2022-02-21 14:06:36 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.devdactic.capacourse',
+  appName: 'capacitorCourse',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
